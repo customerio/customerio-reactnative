4430b66fc6491a4ba7e1c571eb357a318366af3f
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-05-17 15:36:25 +0530

    feat: identify and clear user identity (#2)

--- ios/CustomerioReactnative.swift
+++ ios/CustomerioReactnative.swift
@@ -4,19 +4,42 @@ import CioTracking
 @objc(CustomerioReactnative)
 class CustomerioReactnative: NSObject {
 
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
-    
     @objc static func requiresMainQueueSetup() -> Bool {
-        false
+        false /// false because our native module's initialization does not require access to UIKit
     }
     
+    /**
+     Initialize the package before sending any calls to the package
+     */
     @objc(initialize:apiKey:region:)
     func initialize(siteId: String, apiKey: String, region :String) -> Void {
-        
         CustomerIO.initialize(siteId: siteId, apiKey: apiKey, region: getLocation(from: region))
+    }
+    
+    /**
+     Identify a customer, note that only one customer is identified at a time
+
+     - Parameters:
+     - identifier: unique ID of the customer.
+     - body (Optional): attributes of a customer.
+     */
+    @objc(identify:body:)
+    func identify(identifier: String, body: Dictionary<String, AnyHashable>?) -> Void {
+    
+        guard let data = body else {
+            CustomerIO.shared.identify(identifier: identifier)
+            return
+        }
+        CustomerIO.shared.identify(identifier: identifier, body: data)
+    }
+    
+    /**
+     To stop identifying the user.
+     Once the identity is cleared then the user can not be tracked, hence no events/activities are sent to the workspace
+     */
+    @objc(clearIdentify)
+    func clearIdentify() {
+        CustomerIO.shared.clearIdentify()
     }
     
     private func getLocation(from regionStr : String) -> Region{
@@ -29,4 +52,15 @@ class CustomerioReactnative: NSObject {
             return Region.US
         }
     }
+}
+
+
+extension Encodable {
+  func asDictionary() throws -> [String: Any] {
+    let data = try JSONEncoder().encode(self)
+    guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+      throw NSError()
+    }
+    return dictionary
+  }
 }
