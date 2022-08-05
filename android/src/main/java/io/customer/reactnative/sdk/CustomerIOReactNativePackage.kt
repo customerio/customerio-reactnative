e87c6eb05f5d357a626e982702dc1ff7171e4e09
Muhammad Rehan <mrehan.official@gmail.com>
2022-08-05 21:08:35 +0500

    feat: android: package updates

--- android/src/main/java/io/customer/reactnative/sdk/CustomerIOReactNativePackage.kt
+++ android/src/main/java/io/customer/reactnative/sdk/CustomerIOReactNativePackage.kt
@@ -1,14 +1,15 @@
-package com.customerioreactnative
+package io.customer.reactnative.sdk
 
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
-
-class CustomerioReactnativePackage : ReactPackage {
+class CustomerIOReactNativePackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(CustomerioReactnativeModule(reactContext))
+        return listOf(
+            CustomerIOReactNativeModule(reactContext),
+        )
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
