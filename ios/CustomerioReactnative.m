9e3056b240f2898cad1058d0afc018990dd8d2a4
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-08-09 20:04:13 +0400

    added method in react native bridge

--- ios/CustomerioReactnative.m
+++ ios/CustomerioReactnative.m
@@ -23,4 +23,6 @@ RCT_EXTERN_METHOD(setProfileAttributes : (nonnull NSDictionary *) data)
 RCT_EXTERN_METHOD(screen: (nonnull NSString *) name
                   data : (NSDictionary *) data)
 
+RCT_EXTERN_METHOD(initializeInApp: (nonnull NSString *) organisationId)
+
 @end
