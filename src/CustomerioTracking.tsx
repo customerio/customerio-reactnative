892780203b42bc046ab4d48ffdcab44321eda6fa
Segun <xtreem88@gmail.com>
2022-10-27 17:13:35 +0400

    debug

--- src/CustomerioTracking.tsx
+++ src/CustomerioTracking.tsx
@@ -1,5 +1,9 @@
 import { NativeModules, Platform } from 'react-native';
-import { CustomerioConfig, CustomerIOEnv, PackageConfig } from './CustomerioConfig';
+import {
+  CustomerioConfig,
+  CustomerIOEnv,
+  PackageConfig,
+} from './CustomerioConfig';
 import { Region } from './CustomerioEnum';
 var pjson = require('../package.json');
 
@@ -8,7 +12,6 @@ const LINKING_ERROR =
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
-
 
 /**
  * Get CustomerioReactnative native module
@@ -24,7 +27,6 @@ const CustomerioReactnative = NativeModules.CustomerioReactnative
       }
     );
 
-
 class CustomerIO {
   /**
    * To initialize the package using workspace credentials
@@ -34,90 +36,86 @@ class CustomerIO {
    * @param config set config for the package eg trackApiUrl etc
    * @returns
    */
-   static initialize(env: CustomerIOEnv, config: CustomerioConfig = new CustomerioConfig()) {
+  static initialize(
+    env: CustomerIOEnv,
+    config: CustomerioConfig = new CustomerioConfig()
+  ) {
+    let pversion = pjson.version ?? '';
+    let expoVersion = pjson.expoVersion ?? '1.2.3';
 
-    let pversion = pjson.version ?? ""
-    let expoVersion = pjson.expoVersion ?? "1.2.3"
+    const packageConfig = new PackageConfig();
 
-    const packageConfig = new PackageConfig()
-    packageConfig.source = "ReactNative"
-    packageConfig.version = pversion
-    if (expoVersion != "") {
-      packageConfig.source = "Expo"
-      packageConfig.version = expoVersion
-    }
+    packageConfig.source = 'Expo';
+    packageConfig.version = expoVersion;
 
     console.warn(packageConfig);
 
-    return CustomerioReactnative.initialize(env, config, packageConfig)
+    return CustomerioReactnative.initialize(env, config, packageConfig);
   }
 
-    /**
-     * Identify a person using a unique identifier, eg. email id.
-     * Note that you can identify only 1 profile at a time. In case, multiple
-     * identifiers are attempted to be identified, then the last identified profile
-     * will be removed automatically.
-     *
-     * @param identifier unique identifier for a profile
-     * @param body (Optional) data to identify a profile
-     */
-    static identify(identifier: string, body: Object) {
-      CustomerioReactnative.identify(identifier, body)
-    }
-
-    /**
-     * Call this function to stop identifying a person.
-     *
-     * If a profile exists, clearIdentify will stop identifying the profile.
-     * If no profile exists, request to clearIdentify will be ignored.
-     */
-    static clearIdentify() {
-      CustomerioReactnative.clearIdentify()
-    }
-
-    /**
-     * To track user events like loggedIn, addedItemToCart etc.
-     * You may also track events with additional yet optional data.
-     *
-     * @param name event name to be tracked
-     * @param data (Optional) data to be sent with event
-     */
-    static track(name: string, data : Object) {
-      CustomerioReactnative.track(name, data)
-    }
-
-    /**
-     * Use this function to send custom device attributes
-     * such as app preferences, timezone etc
-     *
-     * @param data device attributes data
-     */
-    static setDeviceAttributes(data : Object) {
-      CustomerioReactnative.setDeviceAttributes(data)
-    }
-
-    /**
-     * Set custom user profile information such as user preference, specific
-     * user actions etc
-     *
-     * @param data additional attributes for a user profile
-     */
-    static setProfileAttributes(data : Object) {
-      CustomerioReactnative.setProfileAttributes(data)
-    }
-
-    /**
-     * Track screen events to record the screens a user visits
-     *
-     * @param name name of the screen user visited
-     * @param data (Optional) any additional data to be sent
-     */
-    static screen(name : string, data : Object) {
-      CustomerioReactnative.screen(name, data)
-    }
+  /**
+   * Identify a person using a unique identifier, eg. email id.
+   * Note that you can identify only 1 profile at a time. In case, multiple
+   * identifiers are attempted to be identified, then the last identified profile
+   * will be removed automatically.
+   *
+   * @param identifier unique identifier for a profile
+   * @param body (Optional) data to identify a profile
+   */
+  static identify(identifier: string, body: Object) {
+    CustomerioReactnative.identify(identifier, body);
   }
 
-  export {
-    CustomerIO,
-    Region
+  /**
+   * Call this function to stop identifying a person.
+   *
+   * If a profile exists, clearIdentify will stop identifying the profile.
+   * If no profile exists, request to clearIdentify will be ignored.
+   */
+  static clearIdentify() {
+    CustomerioReactnative.clearIdentify();
   }
+
+  /**
+   * To track user events like loggedIn, addedItemToCart etc.
+   * You may also track events with additional yet optional data.
+   *
+   * @param name event name to be tracked
+   * @param data (Optional) data to be sent with event
+   */
+  static track(name: string, data: Object) {
+    CustomerioReactnative.track(name, data);
+  }
+
+  /**
+   * Use this function to send custom device attributes
+   * such as app preferences, timezone etc
+   *
+   * @param data device attributes data
+   */
+  static setDeviceAttributes(data: Object) {
+    CustomerioReactnative.setDeviceAttributes(data);
+  }
+
+  /**
+   * Set custom user profile information such as user preference, specific
+   * user actions etc
+   *
+   * @param data additional attributes for a user profile
+   */
+  static setProfileAttributes(data: Object) {
+    CustomerioReactnative.setProfileAttributes(data);
+  }
+
+  /**
+   * Track screen events to record the screens a user visits
+   *
+   * @param name name of the screen user visited
+   * @param data (Optional) any additional data to be sent
+   */
+  static screen(name: string, data: Object) {
+    CustomerioReactnative.screen(name, data);
+  }
+}
+
+export { CustomerIO, Region };
