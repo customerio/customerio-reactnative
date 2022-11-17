4ee211c19bc862d23d272290efaf1f3f7f099357
Segun <xtreem88@gmail.com>
2022-11-17 16:54:10 +0400

    cleanup

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,30 +1,43 @@
 const fs = require('fs');
 
-const filename = `${__dirname}/../../customerio-expo-plugin/package.json`;
-const rnFile = `${__dirname}/../package.json`;
+// set package.json file paths
+const expoPJsonFile = `${__dirname}/../../customerio-expo-plugin/package.json`;
+const rnPJsonFile = `${__dirname}/../package.json`;
 
-if (fs.existsSync(filename)) {
+// check if expo plugin is installed
+if (fs.existsSync(expoPJsonFile)) {
+  // try and catch to revent errors
   try {
-    const pJsonFile = fs.readFileSync(rnFile, 'utf8');
-    const pJson = require(filename);
-    const expoVersionSnippet = `"expoVersion": "${pJson.version}"`;
+    // read package.json file for current package
+    const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
+
+    // import expo plugin's package.json
+    const expoPJson = require(expoPJsonFile);
+
+    // set regular expression for expo version
+    const expoVersionSnippet = `"expoVersion": "${expoPJson.version}"`;
     const versionRegEx = new RegExp(expoVersionSnippet);
 
-    const lines = pJsonFile.split('\n');
-    const missingMmatch = pJsonFile.match(versionRegEx);
-    const expoVersionRegex = /"expoVersion": ".*"/;
-    const existatch = pJsonFile.match(expoVersionRegex);
+    // split package.json lines into array
+    const lines = rnPJson.split('\n');
 
+    const missingMmatch = rnPJson.match(versionRegEx);
+    const expoVersionRegex = /"expoVersion": ".*"/;
+    const existatch = rnPJson.match(expoVersionRegex);
+
+    // check if expoVersion key exists in current package and it has not been set already
     if (existatch && !missingMmatch) {
       const index = lines.findIndex((line) => expoVersionRegex.test(line));
 
+      // set expoVersion to current version in expo plugin package
       const content = [
         ...lines.slice(0, index),
         `  ${expoVersionSnippet},`,
         ...lines.slice(index + 1),
       ];
 
-      fs.writeFileSync(rnFile, content.join('\n'), 'utf8');
+      // save package.json file
+      fs.writeFileSync(rnPJsonFile, content.join('\n'), 'utf8');
     }
-  } catch (error) {}
+  } catch (error) {} // do nothing if this operation fails
 }
