bb214caabf07ec16ce60a2cb654abecfca2bbf1f
Segun <xtreem88@gmail.com>
2022-11-20 12:02:09 +0400

    changed to full path

--- src/helpers/runPostInstall.js
+++ src/helpers/runPostInstall.js
@@ -4,16 +4,16 @@ function runPostInstall() {
   // set package.json file path
   const rnPJsonFile = `${__dirname}/../../package.json`;
 
-    const expoVersion = require('customerio-expo-plugin/version');
+    const expoVersion = require(`${__dirname}/../../../customerio-expo-plugin/lib/commonjs/version`);
     if (expoVersion) {
       const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
 
       const rnPackage = JSON.parse(rnPJson);
       rnPackage.expoVersion = expoVersion.LIB_VERSION;
-      console.log(rnPackage)
+
       fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
     }
-// do nothing if this operation fails
+
 }
 
 exports.runPostInstall = runPostInstall;
