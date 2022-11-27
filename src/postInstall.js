6afd2999e26c39fe4290b9fd8fb0401c53060252
Segun <xtreem88@gmail.com>
2022-11-27 14:32:37 +0400

    fix

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,3 +1,22 @@
-const { runPostInstall } = require('./helpers/runPostInstall');
+const fs = require('fs');
 
-runPostInstall();
+// set package.json file paths
+const expoPJsonFile = `${__dirname}/../../customerio-expo-plugin/package.json`;
+const rnPJsonFile = `${__dirname}/../package.json`;
+
+// check if expo plugin is installed
+if (fs.existsSync(expoPJsonFile)) {
+  // try and catch to revent errors
+  try {
+    // read package.json file for current package
+    const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
+
+    // import expo plugin's package.json
+    const expoPJson = require(expoPJsonFile);
+
+    const rnPackage = JSON.parse(rnPJson);
+    rnPackage.expoVersion = expoPJson.version;
+
+    fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
+  } catch (error) {} // do nothing if this operation fails
+}
