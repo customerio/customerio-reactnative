1bfaa2d3f4a3a783d5c26adef98593f23c102736
Segun <xtreem88@gmail.com>
2022-11-27 16:13:54 +0400

    cleanup

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
-const ex = require('customerio-expo-plugin/version');
 
 // set package.json file paths
 const expoPJsonFile = `${__dirname}/../../customerio-expo-plugin/package.json`;
 const rnPJsonFile = `${__dirname}/../package.json`;
 
-// try and catch to revent errors
+// try and catch to prevent errors
 try {
   // check if expo plugin is installed
   if (fs.existsSync(expoPJsonFile)) {
