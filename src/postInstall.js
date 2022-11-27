1e3c9c45146c0484a030e51848d5d1726221d715
Segun Folowosele <xtreem88@gmail.com>
2022-11-27 16:16:22 +0400

    refactor: modified post install script (#53)
    refactor: modified post install script

--- src/postInstall.js
+++ src/postInstall.js
@@ -4,40 +4,19 @@ const fs = require('fs');
 const expoPJsonFile = `${__dirname}/../../customerio-expo-plugin/package.json`;
 const rnPJsonFile = `${__dirname}/../package.json`;
 
-// check if expo plugin is installed
-if (fs.existsSync(expoPJsonFile)) {
-  // try and catch to revent errors
-  try {
+// try and catch to prevent errors
+try {
+  // check if expo plugin is installed
+  if (fs.existsSync(expoPJsonFile)) {
     // read package.json file for current package
     const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
 
     // import expo plugin's package.json
     const expoPJson = require(expoPJsonFile);
 
-    // set regular expression for expo version
-    const expoVersionSnippet = `"expoVersion": "${expoPJson.version}"`;
-    const versionRegEx = new RegExp(expoVersionSnippet);
+    const rnPackage = JSON.parse(rnPJson);
+    rnPackage.expoVersion = expoPJson.version;
 
-    // split package.json lines into array
-    const lines = rnPJson.split('\n');
-
-    const missingMmatch = rnPJson.match(versionRegEx);
-    const expoVersionRegex = /"expoVersion": ".*"/;
-    const existatch = rnPJson.match(expoVersionRegex);
-
-    // check if expoVersion key exists in current package and it has not been set already
-    if (existatch && !missingMmatch) {
-      const index = lines.findIndex((line) => expoVersionRegex.test(line));
-
-      // set react native SDK expoVersion to current version in expo plugin package
-      const content = [
-        ...lines.slice(0, index),
-        `  ${expoVersionSnippet},`,
-        ...lines.slice(index + 1),
-      ];
-
-      // save react native SDK package.json file
-      fs.writeFileSync(rnPJsonFile, content.join('\n'), 'utf8');
-    }
-  } catch (error) {} // do nothing if this operation fails
-}
+    fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
+  }
+} catch (error) {} // do nothing if this operation fails
