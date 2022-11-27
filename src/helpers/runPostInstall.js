dc42b93657b563ae1f73ba8afbd50c5468faed40
Segun <xtreem88@gmail.com>
2022-11-27 13:59:15 +0400

    fix

--- src/helpers/runPostInstall.js
+++ src/helpers/runPostInstall.js
@@ -4,16 +4,15 @@ function runPostInstall() {
   // set package.json file path
   const rnPJsonFile = `${__dirname}/../../package.json`;
 
-    const expoVersion = require(`${__dirname}/../../../customerio-expo-plugin/lib/commonjs/version`);
-    if (expoVersion) {
-      const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
+  const expoVersion = require(`${__dirname}/../../../customerio-expo-plugin/package.json`);
+  if (expoVersion) {
+    const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
 
-      const rnPackage = JSON.parse(rnPJson);
-      rnPackage.expoVersion = expoVersion.LIB_VERSION;
+    const rnPackage = JSON.parse(rnPJson);
+    rnPackage.expoVersion = expoVersion.version;
 
-      fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
-    }
-
+    fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
+  }
 }
 
 exports.runPostInstall = runPostInstall;
