e063d88834c87050c1abbe12e6d7550068671bd5
Segun Folowosele <xtreem88@gmail.com>
2022-11-30 21:43:29 +0400

    Cleaned up user agent code  (#55)
    Cleaned up user agent code

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,22 +1,4 @@
-const fs = require('fs');
-
-// set package.json file paths
-const expoPJsonFile = `${__dirname}/../../customerio-expo-plugin/package.json`;
-const rnPJsonFile = `${__dirname}/../package.json`;
-
-// try and catch to prevent errors
 try {
-  // check if expo plugin is installed
-  if (fs.existsSync(expoPJsonFile)) {
-    // read package.json file for current package
-    const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
-
-    // import expo plugin's package.json
-    const expoPJson = require(expoPJsonFile);
-
-    const rnPackage = JSON.parse(rnPJson);
-    rnPackage.expoVersion = expoPJson.version;
-
-    fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
-  }
-} catch (error) {} // do nothing if this operation fails
+  const ph = require('customerio-expo-plugin/postinstall');
+  ph.runPostInstall();
+} catch (error) {}
