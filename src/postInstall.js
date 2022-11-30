586504ce47dc2714ccbf36fd0fdd93b7fb7b0a4c
Segun <xtreem88@gmail.com>
2022-11-30 22:12:44 +0400

    cleanup

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,3 +1,9 @@
+/*
+  This code block attempts to set expoVersion in package.json.
+  If it fails, the assumption is that Expo plugin is not installed,
+  therefore we are running on bare React Native project.
+  If it succeeds, we use Expo as the user agent instead of React Native.
+*/
 try {
   const ph = require('customerio-expo-plugin/postinstall');
   ph.runPostInstall();
