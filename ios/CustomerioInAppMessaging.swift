ade61d05a3748fc29f2c428e7839780cec4148ae
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-01-26 17:44:23 +0530

    added in-app event emitter class

--- ios/CustomerioInAppMessaging.swift
+++ ios/CustomerioInAppMessaging.swift
@@ -1,1 +1,11 @@
+import Foundation
+import React
+@objc(CustomerioInAppMessaging)
+class CustomerioInAppMessaging: RCTEventEmitter {
+    public static var shared: CustomerioInAppMessaging?
 
+    override init() {
+        super.init()
+        CustomerioInAppMessaging.shared = self
+    }
+}
