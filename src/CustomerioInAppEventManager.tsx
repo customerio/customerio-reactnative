0c8dc209e3e21190d45a9e28e9ad70ad05469357
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-02-03 01:29:01 +0530

    linking error

--- src/CustomerioInAppEventManager.tsx
+++ src/CustomerioInAppEventManager.tsx
@@ -1,8 +1,12 @@
-import { NativeEventEmitter, NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
 
-
+const LINKING_ERROR =
+  `The package 'customerio-reactnative' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
 /** Code by Rehan
- * Get CustomerIOInAppEventListener native module
+ * Get CustomerioInAppMessaging native module
  */
  const CustomerIOInAppEventListener = NativeModules.CustomerioInAppMessaging
  ? NativeModules.CustomerioInAppMessaging
