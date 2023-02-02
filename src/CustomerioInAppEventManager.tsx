a36ab5b68416a75147f8da7fa72c8f8fb54a0c59
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-02-03 01:27:40 +0530

    check if native module exists

--- src/CustomerioInAppEventManager.tsx
+++ src/CustomerioInAppEventManager.tsx
@@ -1,1 +1,16 @@
-import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
+
+
+/** Code by Rehan
+ * Get CustomerIOInAppEventListener native module
+ */
+ const CustomerIOInAppEventListener = NativeModules.CustomerioInAppMessaging
+ ? NativeModules.CustomerioInAppMessaging
+ : new Proxy(
+   {},
+   {
+     get() {
+       throw new Error(LINKING_ERROR);
+     },
+   }
+ );
