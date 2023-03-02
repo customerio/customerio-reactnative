805245adf47ca69d3140994e39c475012d5bfd77
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-03-02 16:34:33 +0400

    index file

---
+++ src/types/index.tsx
@@ -0,0 +1,10 @@
+/**
+ * This is react native package for Customer.io
+ * Package name : customerio-reactnative
+ */
+
+import { PushConfigOptions } from './PushOptions';
+
+export {
+  PushConfigOptions
+};
