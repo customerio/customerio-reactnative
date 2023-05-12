6405bac00b43ab48cd5088d342a6d27a0d2db5e0
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-05-12 17:36:19 +0530

    removing org/owner header

--- Apps/APN/ios/NotificationServiceExtension/NotificationService.h
+++ Apps/APN/ios/NotificationServiceExtension/NotificationService.h
@@ -1,10 +1,3 @@
-//
-//  NotificationService.h
-//  NotificationServiceExtension
-//
-//  Created by Amandeep Kaur on 08/05/23.
-//
-
 #import <UserNotifications/UserNotifications.h>
 
 @interface NotificationService : UNNotificationServiceExtension
