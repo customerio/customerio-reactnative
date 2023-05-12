9e686a39e84bb379adc76221d95352b4e09c4c5e
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-05-12 17:35:54 +0530

    ios updates

---
+++ Apps/APN/ios/NotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationServiceExtension
+//
+//  Created by Amandeep Kaur on 08/05/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
