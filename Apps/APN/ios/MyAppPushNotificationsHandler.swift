999139686ef2c79fe835f79fb0a1c3e3cdef8e16
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-05-22 20:02:37 +0530

    removed registerPushNotification i.e not in use now

--- Apps/APN/ios/MyAppPushNotificationsHandler.swift
+++ Apps/APN/ios/MyAppPushNotificationsHandler.swift
@@ -8,21 +8,6 @@ public class MyAppPushNotificationsHandler : NSObject {
 
   public override init() {}
 
-  // Skip this function if you don't want to request push permissions on launch
-  @objc(registerPushNotification:)
-  public func registerPushNotification(withNotificationDelegate notificationDelegate: UNUserNotificationCenterDelegate) {
-
-    let center  = UNUserNotificationCenter.current()
-    center.delegate = notificationDelegate
-    center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
-      if error == nil{
-        DispatchQueue.main.async {
-          UIApplication.shared.registerForRemoteNotifications()
-        }
-      }
-    }
-  }
-
   @objc(application:deviceToken:)
   public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     MessagingPush.shared.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
