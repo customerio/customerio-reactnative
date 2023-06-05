96f9dcc1d30bb0583ac6a0f2e48349ecb8af5214
Shahroz Khan <shrozali@gmail.com>
2023-06-05 17:09:46 +0500

    refactor: linting changes and fixes in sample app (#145)

--- Apps/APN/components/Deeplink.js
+++ Apps/APN/components/Deeplink.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import { View, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
 const Deeplinks = () => {
   return (
     <View style={styles.container}>
-        <Text style={styles.textLabel}>
-        Welcome! You see this screen because you tapped on a push notification that had a deep link in it's payload. 
-    </Text>
+      <Text style={styles.textLabel}>
+        Welcome! You see this screen because you tapped on a push notification
+        that had a deep link in it's payload.
+      </Text>
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: "#e3e3e3"
-    },
-    textLabel: {
-        padding: 20,
-        color: '#000',
-        lineHeight: 25
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#e3e3e3',
+  },
+  textLabel: {
+    padding: 20,
+    color: '#000',
+    lineHeight: 25,
+  },
+});
 export default Deeplinks;
--- Apps/APN/index.js
+++ Apps/APN/index.js
@@ -1,9 +1,8 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
-import PushNotification from "react-native-push-notification";
-import {name as appName} from './app.json';
+import PushNotification from 'react-native-push-notification';
+import { name as appName } from './app.json';
 import App from './App';
-
 
 AppRegistry.registerComponent(appName, () => App);
