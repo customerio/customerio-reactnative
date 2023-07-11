0fdb77c31a861ca9e3b6cbb42840fe1edf0af12f
Rehan <mrehan.official@gmail.com>
2023-07-12 01:18:42 +0500

    added StorageService

---
+++ Apps/APN/src/services/StorageService.js
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SDK_CONFIG, USER_STATE } from '../constants/StorageConstants';
+import SDKConfigurations from '../sdk/SDKConfigurations';
+import User from '../src/data/models/user';
+
+export default class StorageService {
+  saveToStorage(key, value) {
+    try {
+      return AsyncStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async loadFromStorage(key) {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      return value ? JSON.parse(value) : null;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  loadSDKConfigurations = async () => {
+    let config = await this.loadFromStorage(SDK_CONFIG);
+    return config ? new SDKConfigurations(config) : null;
+  };
+  saveSDKConfigurations = async (config) =>
+    this.saveToStorage(SDK_CONFIG, config);
+
+  loadUser = async () => {
+    let user = await this.loadFromStorage(USER_STATE);
+    return user ? new User(user.email, user) : null;
+  };
+  saveUser = async (user) => this.saveToStorage(USER_STATE, user);
+}
