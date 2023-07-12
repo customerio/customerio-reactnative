ca788b1f3b5c9061026e10e909d25fa82764a38e
Rehan <mrehan.official@gmail.com>
2023-07-12 18:22:11 +0500

    app screen fixes

--- Apps/APN/App.js
+++ Apps/APN/App.js
@@ -5,59 +5,60 @@ import {
 } from 'customerio-reactnative';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
+import Screen from './src/data/enums/Screen';
 import CustomerIoSDKConfig from './src/data/sdk/CustomerIoSDKConfig';
 import AppNavigator from './src/navigation/AppNavigator';
 import CustomerIOService from './src/services/CustomerIOService';
 import StorageService from './src/services/StorageService';
 
 export default function App() {
-  const storageService = new StorageService();
-
-  const [firstScreen, setFirstScreen] = useState(undefined);
+  const [initialRouteName, setInitialRouteName] = useState(undefined);
   const [loading, setLoading] = useState(true);
-  const [isScreenTrackingEnabled, setScreenTrackingEnabled] = useState(null);
+  const [screenTrackingEnabled, setScreenTrackingEnabled] = useState(null);
+  const [user, setUser] = useState(undefined);
 
   useEffect(() => {
+    const storageService = new StorageService();
+
+    const validateUserState = async () => {
+      const value = await storageService.loadUser();
+      setLoading(false);
+      setUser(value);
+      if (value) {
+        setInitialRouteName(Screen.DASHBOARD.name);
+        return;
+      }
+      setInitialRouteName(Screen.LOGIN.name);
+    };
+
+    const initializeCustomerIoSDK = async () => {
+      const sdkConfig = CustomerIoSDKConfig.applyDefaultForUndefined(
+        await storageService.loadSDKConfigurations()
+      );
+
+      const env = new CustomerIOEnv();
+      env.siteId = sdkConfig.siteId;
+      env.apiKey = sdkConfig.apiKey;
+
+      const config = new CustomerioConfig();
+      if (sdkConfig.debugMode) {
+        config.logLevel = CioLogLevel.debug;
+      }
+      if (sdkConfig.trackingApiUrl) {
+        config.trackingApiUrl = sdkConfig.trackingApiUrl;
+      }
+      config.autoTrackDeviceAttributes = sdkConfig.trackDeviceAttributes;
+      config.backgroundQueueMinNumberOfTasks = sdkConfig.bqMinNumberOfTasks;
+      config.backgroundQueueSecondsDelay = sdkConfig.bqSecondsDelay;
+      setScreenTrackingEnabled(sdkConfig.trackScreens);
+
+      const customerIOService = new CustomerIOService();
+      customerIOService.initializeSDK(env, config);
+    };
+
     validateUserState();
     initializeCustomerIoSDK();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const validateUserState = async () => {
-    const user = await storageService.loadUser();
-    setLoading(false);
-    if (user) {
-      setFirstScreen('Dashboard');
-      return;
-    }
-    setFirstScreen('Login');
-  };
-
-  const initializeCustomerIoSDK = async () => {
-    const sdkConfig = CustomerIoSDKConfig.applyDefaultForUndefined(
-      await storageService.loadSDKConfigurations()
-    );
-
-    const env = new CustomerIOEnv();
-    env.siteId = sdkConfig.siteId;
-    env.apiKey = sdkConfig.apiKey;
-
-    const config = new CustomerioConfig();
-    if (sdkConfig.debugMode) {
-      config.logLevel = CioLogLevel.debug;
-    }
-    if (sdkConfig.trackingApiUrl) {
-      config.trackingApiUrl = sdkConfig.trackingApiUrl;
-    }
-    config.autoTrackDeviceAttributes = sdkConfig.trackDeviceAttributes;
-    config.backgroundQueueMinNumberOfTasks = sdkConfig.bqMinNumberOfTasks;
-    config.backgroundQueueSecondsDelay = sdkConfig.bqSecondsDelay;
-    setScreenTrackingEnabled(sdkConfig.trackScreens);
-
-    const customerIOService = new CustomerIOService();
-    customerIOService.initializeSDK(env, config);
-  };
 
   if (loading) {
     return <ActivityIndicator />;
@@ -65,8 +66,9 @@ export default function App() {
 
   return (
     <AppNavigator
-      firstScreen={firstScreen}
-      isScreenTrackingEnabled={isScreenTrackingEnabled}
+      initialRouteName={initialRouteName}
+      screenTrackingEnabled={screenTrackingEnabled}
+      user={user}
     />
   );
 }
