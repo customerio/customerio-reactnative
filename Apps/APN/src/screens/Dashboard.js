a5732dc96cde018a3cb73bc32fada97983f13b09
Rehan <mrehan.official@gmail.com>
2023-07-13 00:27:48 +0500

    updated dashboard

---
+++ Apps/APN/src/screens/Dashboard.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import BuildInfoText from '../components/BuildInfoText';
+import * as Colors from '../constants/Colors';
+import * as Fonts from '../constants/Fonts';
+import * as Sizes from '../constants/Sizes';
+import Screen from '../data/enums/Screen';
+import CustomerIOService from '../services/CustomerIOService';
+import StorageService from '../services/StorageService';
+
+const Dashboard = ({ navigation, user }) => {
+  const customerIOService = new CustomerIOService();
+
+  const handleSettingsPress = () => {
+    navigation.navigate(Screen.SETTINGS.name);
+  };
+
+  const handleButtonClick = async (action) => {
+    switch (action) {
+      case ActionItem.RANDOM_EVENT:
+        break;
+
+      case ActionItem.SHOW_PUSH_PROMPT:
+        break;
+
+      case ActionItem.SIGN_OUT:
+        const storageService = new StorageService();
+        await storageService.clearUser();
+        await customerIOService.clearUserIdentity();
+        navigation.navigate(Screen.LOGIN.name);
+        break;
+
+      case ActionItem.CUSTOM_EVENT:
+      case ActionItem.DEVICE_ATTRIBUTES:
+      case ActionItem.PROFILE_ATTRIBUTES:
+        navigation.navigate(action.targetScreen.name);
+        break;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <TouchableOpacity
+          style={styles.settingsButton}
+          onPress={handleSettingsPress}
+        >
+          <Image
+            source={require('../../assets/images/black-settings-button.png')}
+            style={styles.settingsIcon}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.spaceTop} />
+
+        <View style={styles.content}>
+          <Text style={styles.email}>{user.email}</Text>
+          <Text style={styles.title}>What would you like to test?</Text>
+          {Object.values(ActionItem).map((action) => (
+            <TouchableOpacity
+              key={action.text}
+              style={styles.actionButton}
+              onPress={() => handleButtonClick(action)}
+            >
+              <Text style={styles.actionButtonText}>{action.text}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <View style={styles.spaceBottom} />
+
+        <BuildInfoText style={styles.footerText} />
+      </ScrollView>
+    </View>
+  );
+};
+
+const ActionItem = {
+  RANDOM_EVENT: {
+    text: 'Send Random Event',
+    contentDesc: 'Random Event Button',
+    targetScreen: null,
+  },
+  CUSTOM_EVENT: {
+    text: 'Send Custom Event',
+    contentDesc: 'Custom Event Button',
+    targetScreen: Screen.CUSTOM_EVENTS,
+  },
+  DEVICE_ATTRIBUTES: {
+    text: 'Set Device Attribute',
+    contentDesc: 'Device Attribute Button',
+    targetScreen: Screen.DEVICE_ATTRIBUTES,
+  },
+  PROFILE_ATTRIBUTES: {
+    text: 'Set Profile Attribute',
+    contentDesc: 'Profile Attribute Button',
+    targetScreen: Screen.PROFILE_ATTRIBUTES,
+  },
+  SHOW_PUSH_PROMPT: {
+    text: 'Show Push Prompt',
+    contentDesc: 'Show Push Prompt Button',
+    targetScreen: null,
+  },
+  SIGN_OUT: {
+    text: 'Log Out',
+    contentDesc: 'Log Out Button',
+    targetScreen: null,
+  },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.CONTAINER_BACKGROUND_COLOR,
+    flex: 1,
+  },
+  topBar: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: Sizes.TOP_BAR_HEIGHT,
+    justifyContent: 'flex-end',
+    paddingHorizontal: Sizes.TOP_BAR_PADDING_HORIZONTAL,
+  },
+  settingsButton: {
+    padding: Sizes.IMAGE_BUTTON_PADDING,
+  },
+  settingsIcon: {
+    width: Sizes.IMAGE_BUTTON_ICON_SIZE,
+    height: Sizes.IMAGE_BUTTON_ICON_SIZE,
+  },
+  spaceTop: {
+    flex: 1,
+  },
+  spaceBottom: {
+    flex: 1,
+  },
+  content: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  email: {
+    alignSelf: 'center',
+    color: Colors.TEXT_COLOR_PRIMARY,
+    fontFamily: Fonts.FONT_FAMILY_REGULAR,
+    fontSize: 20,
+    fontWeight: Colors.FONT_WEIGHT_REGULAR,
+    marginBottom: 16,
+    marginTop: 16,
+  },
+  title: {
+    alignSelf: 'center',
+    color: Colors.TEXT_COLOR_PRIMARY,
+    fontFamily: Fonts.FONT_FAMILY_REGULAR,
+    fontSize: 20,
+    fontWeight: Colors.FONT_WEIGHT_REGULAR,
+    marginBottom: 16,
+  },
+  actionButton: {
+    backgroundColor: Colors.PRIMARY_COLOR,
+    borderRadius: Sizes.BUTTON_BORDER_RADIUS,
+    paddingHorizontal: Sizes.BUTTON_PADDING_HORIZONTAL,
+    paddingVertical: Sizes.BUTTON_PADDING_VERTICAL,
+    marginTop: 16,
+    maxWidth: Sizes.BUTTON_MAX_WIDTH,
+    width: '80%',
+  },
+  actionButtonText: {
+    color: Colors.TEXT_COLOR_ON_PRIMARY,
+    fontFamily: Fonts.FONT_FAMILY_REGULAR,
+    fontWeight: Fonts.FONT_FAMILY_BOLD,
+    textAlign: 'center',
+  },
+  footer: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+});
+
+export default Dashboard;
--- Apps/APN/src/services/StorageService.js
+++ Apps/APN/src/services/StorageService.js
@@ -1,21 +1,29 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SDK_CONFIG, USER_STATE } from '../constants/StorageConstants';
-import CustomerIoSDKConfig from '../data/sdk/CustomerIoSDKConfig';
 import User from '../data/models/user';
+import CustomerIoSDKConfig from '../data/sdk/CustomerIoSDKConfig';
 
 export default class StorageService {
-  saveToStorage(key, value) {
+  async loadFromStorage(key) {
     try {
-      return AsyncStorage.setItem(key, JSON.stringify(value));
+      const value = await AsyncStorage.getItem(key);
+      return value ? JSON.parse(value) : null;
     } catch (e) {
       console.log(e);
     }
   }
 
-  async loadFromStorage(key) {
+  removeFromStorage(key, value) {
     try {
-      const value = await AsyncStorage.getItem(key);
-      return value ? JSON.parse(value) : null;
+      return AsyncStorage.removeItem(key);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  saveToStorage(key, value) {
+    try {
+      return AsyncStorage.setItem(key, JSON.stringify(value));
     } catch (e) {
       console.log(e);
     }
@@ -33,4 +41,5 @@ export default class StorageService {
     return user ? new User(user.email, user) : null;
   };
   saveUser = async (user) => this.saveToStorage(USER_STATE, user);
+  clearUser = async () => this.saveToStorage(USER_STATE, null);
 }
