7615c8274fc64f7002fa6043f1f493bd500666b9
Rehan <mrehan.official@gmail.com>
2023-07-14 02:08:57 +0500

    removed unused method

--- Apps/APN/src/utils/navigation.js
+++ Apps/APN/src/utils/navigation.js
@@ -4,14 +4,6 @@ export const navigateToScreen = (navigation, screen, params = {}) => {
   navigation.navigate(screen.name, params);
 };
 
-export const getLocation = (screen) => {
-  if (screen === Screen.DASHBOARD || screen === Screen.LOGIN) {
-    return screen.path;
-  } else {
-    return `${Screen.DASHBOARD.path}${screen.path}`;
-  }
-};
-
 export const isAuthenticatedViewOnly = (screen) => {
   return !isUnauthenticatedViewOnly(screen) && !isPublicViewAllowed(screen);
 };
