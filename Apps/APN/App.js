8047171e4e1fa7e048e8f93c8429452cf60c1a3a
Rehan <mrehan.official@gmail.com>
2023-07-20 13:15:55 +0500

    fixed array

--- Apps/APN/App.js
+++ Apps/APN/App.js
@@ -29,7 +29,7 @@ export default function App() {
     };
 
     prepare();
-    const listeners = registerInAppEventListener();
+    const listeners = [registerInAppEventListener()];
 
     // Remove listeners once unmounted
     return () => {
