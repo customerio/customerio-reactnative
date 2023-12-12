879e12a73062e546aa8306294615d0a3fcc7f9ef
Levi Bostian <levi@customer.io>
2023-12-12 12:06:45 -0600

    remove more native code that is no longer needed with new feature

--- Apps/FCM/ios/FCMSampleApp/AppDelegate.h
+++ Apps/FCM/ios/FCMSampleApp/AppDelegate.h
@@ -1,8 +1,7 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-#import <UserNotifications/UserNotifications.h>
 #import <FirebaseMessaging/FIRMessaging.h>
 
-@interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
+@interface AppDelegate : RCTAppDelegate <FIRMessagingDelegate>
 
 @end
