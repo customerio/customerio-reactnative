92109501b9b87ed4fecabbd06d7a29d9f32ffc43
Levi Bostian <levi@customer.io>
2024-07-18 15:44:52 -0500

    POC for executing code when didFinishLaunching is called.

--- ios/CustomerioReactnative.m
+++ ios/CustomerioReactnative.m
@@ -37,3 +37,54 @@ RCT_EXTERN_METHOD(trackNotificationResponseReceived : (nonnull NSDictionary *) p
 RCT_EXTERN_METHOD(trackNotificationReceived : (nonnull NSDictionary *) payload])
 
 @end
+
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+
+@implementation UIApplication(CIOReactNative) 
+
+static void injectSelector(Class newClass, SEL newSel, Class addToClass, SEL makeLikeSel) {
+    Method newMeth = class_getInstanceMethod(newClass, newSel);
+    IMP imp = method_getImplementation(newMeth);
+    const char* methodTypeEncoding = method_getTypeEncoding(newMeth);
+
+    BOOL successful = class_addMethod(addToClass, makeLikeSel, imp, methodTypeEncoding);
+    if (!successful) {
+        class_addMethod(addToClass, newSel, imp, methodTypeEncoding);
+        newMeth = class_getInstanceMethod(addToClass, newSel);
+
+        Method orgMeth = class_getInstanceMethod(addToClass, makeLikeSel);
+
+        method_exchangeImplementations(orgMeth, newMeth);
+    }
+}
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        method_exchangeImplementations(class_getInstanceMethod(self, @selector(setDelegate:)), class_getInstanceMethod(self, @selector(setCioReactNativeDelegate:)));
+    });
+}
+
+- (void) setCioReactNativeDelegate:(id<UIApplicationDelegate>)delegate {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class delegateClass = [delegate class];
+        injectSelector(self.class, @selector(cio_application:didFinishLaunchingWithOptions:),
+                       delegateClass, @selector(application:didFinishLaunchingWithOptions:));
+        
+        [self setCioReactNativeDelegate:delegate]; // continue swizzle
+    });
+}
+
+- (BOOL)cio_application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+    NSLog(@"Called cio_applicationDidFinishLaunching: %@", launchOptions);
+    
+    // continue swizzle
+    if ([self respondsToSelector:@selector(cio_application:didFinishLaunchingWithOptions:)])
+        return [self cio_application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    return YES;
+}
+
+@end
