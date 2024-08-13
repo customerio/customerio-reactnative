42090c0610022100664ada16bda58b0e7ef1a4d6
ami-aman <91549653+ami-aman@users.noreply.github.com>
2024-08-13 17:29:05 +0400

    adding meaningful comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 /**
- * This is react native package for Customer.io
- * Package name : customerio-reactnative
+ * This file serves as the entry point for the module customerio-reactnative.
  */
 export * from './cio-config';
 export * from './customerio-cdp';
