7bd59175ee560b330ae3ce815f8660c81f13a3ae
ami-aman <91549653+ami-aman@users.noreply.github.com>
2024-08-14 16:31:53 +0400

    native file updates

--- ios/StringExtensions.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-internal extension StringProtocol {
-    // Uppercase the first character without modifying the rest of the string.
-    // "notDeteremined" -> "NotDetermined"
-    // "notdetermined" -> "Notdeteremined"
-    // "not determined" -> "Not determined"
-    var firstUppercased: String { return prefix(1).uppercased() + dropFirst() }
-}
