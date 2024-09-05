74240befaddb99c78bcc4673913ca2ce5d15d74f
ami-aman <91549653+ami-aman@users.noreply.github.com>
2024-09-05 15:22:52 +0530

    CustomerIOReactNativeLoggingWrapper class

--- android/src/main/java/io/customer/reactnative/sdk/logging/CustomerIOReactNativeLogger.kt
+++ android/src/main/java/io/customer/reactnative/sdk/logging/CustomerIOReactNativeLogger.kt
@@ -3,8 +3,11 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.modules.core.DeviceEventManagerModule
+import io.customer.sdk.core.di.SDKComponent
+import io.customer.sdk.core.util.CioLogLevel
+import io.customer.sdk.core.util.Logger
 
-class CioLoggingEmitter(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class CustomerIOReactNativeLoggingEmitter(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
     companion object {
         const val EVENT_NAME = "CioLogEvent"
@@ -47,4 +50,48 @@ class CioLoggingEmitter(reactContext: ReactApplicationContext) : ReactContextBas
     }
 }
 
+class CustomerIOReactNativeLoggingWrapper private constructor(
+    private var moduleRegistry: ReactApplicationContext,
+    override var logLevel: CioLogLevel): Logger {
 
+    companion object {
+        fun getInstance(moduleRegistry: ReactApplicationContext, logLevel: CioLogLevel): CustomerIOReactNativeLoggingWrapper {
+            val instance = CustomerIOReactNativeLoggingWrapper(moduleRegistry, logLevel)
+//            DIGraphShared.shared.override(instance, CioLogger::class.java)
+            return instance
+        }
+    }
+
+//    fun setLogLevel(level: CioLogLevel) {
+//        logLevel = level
+//    }
+
+    override fun debug(message: String) {
+        emit(message, CioLogLevel.DEBUG)
+    }
+
+    override fun info(message: String) {
+        emit(message, CioLogLevel.INFO)
+    }
+
+    override fun error(message: String) {
+        emit(message, CioLogLevel.ERROR)
+    }
+
+    private fun emit(message: String, level: CioLogLevel) {
+        if (shouldEmit(level)) {
+            val emitter = moduleRegistry.getNativeModule(CustomerIOReactNativeLoggingEmitter::class.java)
+            emitter?.sendEvent(level.name, message)
+        }
+    }
+
+    private fun shouldEmit(level: CioLogLevel): Boolean {
+        return when (logLevel) {
+            CioLogLevel.NONE -> false
+            CioLogLevel.ERROR -> level == CioLogLevel.ERROR
+            CioLogLevel.INFO -> level == CioLogLevel.ERROR || level == CioLogLevel.INFO
+            CioLogLevel.DEBUG -> true
+        }
+    }
+
+}
