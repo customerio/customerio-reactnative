2feb56f2ba1f90550ce80bf4aaf221f1febcdd85
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2025-03-27 21:26:03 +0200

    Fix Android compilation error for RN 0.78.x

--- android/src/main/java/io/customer/reactnative/sdk/extension/MapExtensions.kt
+++ android/src/main/java/io/customer/reactnative/sdk/extension/MapExtensions.kt
@@ -3,7 +3,7 @@ package io.customer.reactnative.sdk.extension
 import com.facebook.react.bridge.ReadableMap
 
 internal fun ReadableMap?.toMap(): Map<String, Any> {
-    return this?.toHashMap()?.filterValues { it != null } ?: emptyMap()
+    return this?.toHashMap()?.filterValues { it != null }?.mapValues { it.value as Any } ?: emptyMap()
 }
 
 internal inline fun <reified V> Map<String, Any?>?.getTypedValue(key: String): V? {
