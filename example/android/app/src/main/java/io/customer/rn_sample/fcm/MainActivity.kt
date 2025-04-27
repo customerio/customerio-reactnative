cf2160aae081709c5ba49f6c7c9b23881320d9fc
Ahmed-Ali <eng.ahmed.ali.awad@gmail.com>
2025-04-27 19:01:32 +0400

    chore: New sample app Android native code

---
+++ example/android/app/src/main/java/io/customer/rn_sample/fcm/MainActivity.kt
@@ -0,0 +1,39 @@
+package io.customer.rn_sample.fcm
+
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+import io.customer.rn_sample.fcm.R
+
+/**
+ * Subclassing to override getLaunchOptions
+ * This is needed to pass the appName to be displayed
+ * in the app footer
+ */
+class ReactDelegate public constructor(activity: ReactActivity, mainComponentName: String, fabricEnabled: Boolean = true) :
+    DefaultReactActivityDelegate(activity, mainComponentName, fabricEnabled) {
+
+    override fun getLaunchOptions(): Bundle {
+        val options = Bundle()
+        options.putString("appName", mainComponentName)
+        return options
+    }
+}
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Android FCM"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      ReactDelegate(this, mainComponentName, fabricEnabled)
+}
