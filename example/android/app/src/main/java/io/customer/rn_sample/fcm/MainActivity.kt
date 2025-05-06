218408a1f7666e4ebfff23f5e5eed9e2a3aa2d02
Ahmed-Ali <eng.ahmed.ali.awad@gmail.com>
2025-05-06 21:24:27 +0400

    Updated the sample app names for better consistency and iOS app icon

--- example/android/app/src/main/java/io/customer/rn_sample/fcm/MainActivity.kt
+++ example/android/app/src/main/java/io/customer/rn_sample/fcm/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Android FCM"
+  override fun getMainComponentName(): String = "React Native Android"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
