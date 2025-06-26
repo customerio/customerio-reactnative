a3d6489a5e38a7314a5c4290ff7cecbfddb536c7
Muhammad Rehan <mrehan.official@gmail.com>
2025-06-26 20:58:47 +0500

    chore: ios view manager with new architecture support (#449)

---
+++ ios/wrappers/inapp/inline/RCTInlineMessageNative.h
@@ -0,0 +1,18 @@
+#ifdef RCT_NEW_ARCH_ENABLED
+
+#import <React/RCTViewComponentView.h>
+#import <UIKit/UIKit.h>
+
+#ifndef RCTInlineMessageNative_h
+#define RCTInlineMessageNative_h
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTInlineMessageNative : RCTViewComponentView
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif /* RCTInlineMessageNative_h */
+
+#endif
